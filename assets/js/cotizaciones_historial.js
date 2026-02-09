1343b6c6f7b184c3fe8982be70d3570c0aa36f19
MikeySeregon <82459971+MikeySeregon@users.noreply.github.com>
2026-02-08 20:15:56 -0600

    Hotfix

--- assets/js/cotizaciones_historial.js
+++ assets/js/cotizaciones_historial.js
@@ -259,7 +259,7 @@ window.generarPDFHistorial = async function (idCotizacion) {
 		// ================= Cabecera =================
 		const { data: cab, error: errCab } = await supabase
 			.from('cotizaciones')
-			.select('*,cliente:clientes(rtn)')
+			.select('*,cliente:clientes(nombre,rtn)')
 			.eq('id_cotizacion', idCotizacion)
 			.single();
 
@@ -276,7 +276,7 @@ window.generarPDFHistorial = async function (idCotizacion) {
 			id_cotizacion: cab.id_cotizacion,
 			fecha: cab.fecha_cotizacion,
 			cliente: {
-				nombre: cab.cliente,
+				nombre: cab.cliente.nombre,
 				direccion: cab.direccion,
 				rtn: cab.cliente.rtn
 			},
