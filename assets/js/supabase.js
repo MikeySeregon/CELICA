4aca4c3e31ccde050e890a958a8d68a5fd2461ef
MikeySeregon <82459971+MikeySeregon@users.noreply.github.com>
2026-04-07 11:05:57 -0600

    API

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://filydfpcxictckxwqyyk.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbHlkZnBjeGljdGNreHdxeXlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NTIwMjgsImV4cCI6MjA4MzIyODAyOH0.ybW8gwNh1ngRZDlikebUxFh4mPHraIWVBKMTfCzS8No'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbHlkZnBjeGljdGNreHdxeXlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NzY1MjAyOCwiZXhwIjoyMDgzMjI4MDI4fQ.-13Pv8r6PS0JKVTp9TjxdtyGSRS5JHNq3KOB-YJdG-4'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
